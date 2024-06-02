38d35ef98e12155bae111d9f1ed898933f09ab9c
Pawel Protas <contact@pawelprotas.com>
2024-06-02 16:42:38 +0200

    obsidian improvements

--- lua/util/obsidian.lua
+++ lua/util/obsidian.lua
@@ -2,6 +2,11 @@
 local M = {}
 
 function M.open()
+  if vim.fn.has("macunix") == 1 then
+    vim.cmd("!open -g -a /Applications/Obsidian.app 'obsidian://open?vault=notes&file=%'")
+    return
+  end
+
   vim.cmd("!xdg-open 'obsidian://open?vault=notes&file=%'")
 end
 
